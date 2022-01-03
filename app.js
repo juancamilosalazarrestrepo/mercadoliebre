8d85bf8e7e70cc08b05ba2db3d78e357859fa54d
juan salazar <juancamilosalazarrestrepo@gmail.com>
2022-01-02 20:35:18 -0500

    login y register
    login register y search input

--- app.js
+++ app.js
@@ -9,5 +9,7 @@ const publicPath = path.resolve(__dirname,'./public');
 app.use(express.static(publicPath));
 
 app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, './views/home.html'))  )
-
+app.post('/', (req,res) => res.sendFile(path.resolve(__dirname, './views/home.html'))  )
+app.get('/registro', (req,res) => res.sendFile(path.resolve(__dirname, './views/register.html'))  )
+app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname, './views/login.html'))  )
 app.listen(process.env.PORT || 3000, () =>console.log("Servidor Corriendo en Puerto 3000"));
