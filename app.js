f30e14f06f3ec106ab8b3409b6ae97401f6b007a
juan salazar <juancamilosalazarrestrepo@gmail.com>
2022-01-08 16:33:13 -0500

    arreglos en css
    arregle el tamano de la letra en el nav y el tamano del logo

--- app.js
+++ app.js
@@ -12,4 +12,4 @@ app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, './views/home.htm
 app.post('/', (req,res) => res.sendFile(path.resolve(__dirname, './views/home.html'))  )
 app.get('/registro', (req,res) => res.sendFile(path.resolve(__dirname, './views/register.html'))  )
 app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname, './views/login.html'))  )
-app.listen(process.env.PORT || 3000, () =>console.log("Servidor Corriendo en Puerto 3000"));
+app.listen(process.env.PORT || 3000, () =>console.log("Servidor Corriendo"));
